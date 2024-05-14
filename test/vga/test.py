8e024a0ea9fbd01d88df526b0922ea36bcae1b71
Gerrit Fabian Grutzeck <ggrutzeck@mpifr-bonn.mpg.de>
2024-05-14 13:04:09 +0200

    Setup basic test infrastructure

---
+++ test/vga/test.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: MIT
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+
+
+@cocotb.test()
+async def test_project(dut):
+    dut._log.info("Start")
+
+    # Set the clock period to 25 ns (40 MHz)
+    clock = Clock(dut.clk, 25, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Reset
+    dut._log.info("Reset")
+    dut.rst_n.value = 0
+    dut.pixel_div = 10
+    dut.frame_pixel = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+
+    dut._log.info("Test project behavior")
+
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.gray.value == 0
+
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
