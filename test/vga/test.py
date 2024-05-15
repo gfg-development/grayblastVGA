4d57af3a8e8a611952761a56cd722482b98ef70a
Gerrit Fabian Grutzeck <ggrutzeck@mpifr-bonn.mpg.de>
2024-05-15 17:43:09 +0200

    Extending the VGA test

--- test/vga/test.py
+++ test/vga/test.py
@@ -3,11 +3,11 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge
+from cocotb.utils import get_sim_time
 
 @cocotb.test()
-async def test_project(dut):
+async def test_durations(dut):
     dut._log.info("Start")
 
     # Set the clock period to 25 ns (40 MHz)
@@ -18,18 +18,38 @@ async def test_project(dut):
     dut._log.info("Reset")
     dut.rst_n.value = 0
     dut.pixel_div = 10
-    dut.frame_pixel = 0
+    dut.frame_pixel = 1
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # Measure v_sync length
+    await RisingEdge(dut.v_sync)
+    start_v_sync = get_sim_time("ns")
+    await FallingEdge(dut.v_sync)
+    
+    assert get_sim_time("ns") - start_v_sync == 105600
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 600) # * 800 * 4)
+    for _ in range(600):
+        # Measure h_sync length
+        await RisingEdge(dut.h_sync)
+        start_h_sync = get_sim_time("ns")
+        await FallingEdge(dut.h_sync)
+        assert get_sim_time("ns") - start_h_sync == 3200
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.gray.value == 0
+        await ClockCycles(dut.clk, 87)
+        assert dut.gray.value == 0
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        for _ in range(800):
+            await ClockCycles(dut.clk, 1)
+            assert dut.gray.value == 1
+
+        await ClockCycles(dut.clk, 0)
+        assert dut.gray.value == 0
+
+        # Measure length of line
+        await RisingEdge(dut.h_sync)   
+        assert get_sim_time("ns") - start_h_sync == 26400
+    
+    # Measure frame length
+    await RisingEdge(dut.v_sync)    
+    assert get_sim_time("ns") - start_v_sync == 16579200
