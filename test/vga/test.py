3cc153b57ffd33e5bb100aa2af92def5d19cd81d
Gerrit Fabian Grutzeck <ggrutzeck@mpifr-bonn.mpg.de>
2024-05-15 17:12:25 +0200

    Fixing status of test report

--- test/vga/test.py
+++ test/vga/test.py
@@ -25,7 +25,7 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 600 * 800)
+    await ClockCycles(dut.clk, 600) # * 800 * 4)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
